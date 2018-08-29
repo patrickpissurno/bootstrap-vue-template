769a20c561041ab6a050b042bc76c7cbdc4e9da9
Patrick Pissurno <contato@patrickpissurno.com.br>
2018-08-29 15:55:41 -0300

    Initial commit

---
+++ src/pages/Home.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <navbar active="home"></navbar>
+        <div class="page d-flex flex-column justify-content-center align-items-center">
+            <h1>Bootstrap-vue Template</h1>
+            <p class="mb-0">Yes, it works! Now you can start developing your next project with ease</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        data() {
+            return {
+
+            };
+        }
+    };
+</script>
+
+<style scoped>
+    .page {
+        min-height: calc(100vh - 98px);
+        margin-top: 98px;
+    }
+    h1 {
+        font-weight: 300;
+        font-size: 2.4rem;
+        user-select: none;
+        cursor: default;
+    }
+</style>
---
+++ src/pages/Page2.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <navbar active="page2"></navbar>
+        <div class="page d-flex flex-column justify-content-center align-items-center">
+            <h1>Page 2</h1>
+            <p class="mb-0">{{ text }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Page2",
+        data() {
+            return {
+                text: 'This is another page'
+            };
+        },
+        mounted(){
+
+        },
+        beforeDestroy(){
+
+        },
+        methods: {
+
+        }
+    };
+</script>
+
+<style scoped>
+    .page {
+        min-height: calc(100vh - 98px);
+        margin-top: 98px;
+    }
+    h1 {
+        font-weight: 300;
+        font-size: 2.4rem;
+        user-select: none;
+        cursor: default;
+    }
+</style>
